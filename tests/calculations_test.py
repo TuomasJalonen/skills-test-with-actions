105c3faac389b80ceeaee65935173f7ca8e66e8e
Tuomas Jalonen <tuomas.a.jalonen@gmail.com>
2026-05-21 07:08:13 +0000

    test: add unit test for negative fibonacci sequence

--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -42,6 +42,15 @@ def test_area_of_circle_negative_radius():
    with pytest.raises(ValueError):
       area_of_circle(radius)
 
+def test_get_nth_fibonacci_negative():
+   """Test with a negative number to raise ValueError."""
+   # Arrange
+   n = -1
+
+   # Act & Assert
+   with pytest.raises(ValueError):
+      get_nth_fibonacci(n)
+
 def test_get_nth_fibonacci_zero():
     """Test with n=0."""
     # Arrange
